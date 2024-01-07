6fe7c1a16172ddd30a3eba2fdf13cde99ad36e70
nhyeonjeong <hjnam5962@gmail.com>
2024-01-08 02:25:04 +0900

    Initial Commit

---
+++ TableViewPracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPracticeProject
+//
+//  Created by 남현정 on 2024/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
